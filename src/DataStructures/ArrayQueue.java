8323a6283ce133e95964a37688bfd8405227ed60
captcrunchjr <82350352+captcrunchjr@users.noreply.github.com>
2023-03-12 19:51:15 -0400

    tests pass

--- src/DataStructures/ArrayQueue.java
+++ src/DataStructures/ArrayQueue.java
@@ -1,8 +1,71 @@
 package DataStructures;
 
+import java.util.Arrays;
+
 import ADTs.*;
 import Exceptions.*;
 
-public class ArrayQueue {
+public class ArrayQueue<T> implements QueueADT<T> {
+
+    int size=0;
+    int cap;
+    T q[];
+
+    public ArrayQueue(){
+        cap = 10;
+    }
+
+    public ArrayQueue(int cap){
+        this.cap = cap;
+        q = (T[]) new Object[cap];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size <= 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void enqueue(T element) throws QueueOverflowException {
+        if(size+1 >= cap){
+            throw new QueueOverflowException();
+        }
+        q[size] = element;
+        size++;
+    }
+
+    @Override
+    public T dequeue() throws EmptyCollectionException {
+        if(isEmpty()){
+            throw new EmptyCollectionException();
+        }
+        T[] tempStorage = (T[]) new Object[cap];
+        T result = q[0];
+        int count = 0;
+        q[0] = null;
+        for(int i = 0; i<size; i++){
+            if(q[i]!=null){
+                tempStorage[count] = q[i];
+                count++;
+            }
+        }
+        q = Arrays.copyOf(tempStorage, cap);
+        size--;
+        return result;
+    }
+
+    @Override
+    public T peek() throws EmptyCollectionException {
+        if(isEmpty()){
+            throw new EmptyCollectionException();
+        }
+        return q[0];
+    }
+
     
 }
